a64fd6a6dc3acf8d415c68c234781cac1dd9a62f
nithinvisva <visvanic@gmailcom>
2022-08-01 13:19:09 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
+const record = require('./routes/record')
+const router = require('./routes/users')
 const port = process.env.PORT || 3000
 
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello World!" })
 })
+
+app.use('/record',auth.auth, record);
+app.use('/users', router);
 
 app.listen(port, () => {
   console.log(`Example app listening at Port: ${port}`)
